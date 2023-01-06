c777e2ba7271017b82675966f59637696122a6f6
balbano <brendan@brendanalbano.com>
2023-01-05 21:47:23 -0800

    Improve logo image script

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
+"use strict";
+
+// src values to rotate through when you click on the logo.
+const logoSrcs = [
+    "assets/logo_rotation/logo_600x600.png",
+    "assets/logo_rotation/familiar_600x600.png",
+    "assets/logo_rotation/familiar_closeup_600x600.png"
+];
+var logoSrcIndex = 0;
+
+// Go to the next image in the array of srcs.
 function nextLogoImage(evt) {
-    evt.preventDefault(); // Prevents both both a mouse and touch event from being delivered.
-    const logoImage = document.getElementById("logo");
-    const sources = [
-        "assets/logo_rotation/logo_600x600.png",
-        "assets/logo_rotation/familiar_600x600.png",
-        "assets/logo_rotation/familiar_closeup_600x600.png"
-    ];
-    switch(logoImage.getAttribute("src")) {
-        case sources[0]:
-            logoImage.src=sources[1];
-            break;
-        case sources[1]:
-            logoImage.src=sources[2];
-            break;
-        default:
-            logoImage.src=sources[0];
-    }
+    evt.preventDefault(); // Prevents mouse+touch double click.
+    logoSrcIndex = (logoSrcIndex + 1) % logoSrcs.length; // Wrap back to 0.
+    document.getElementById("logo").src = logoSrcs[logoSrcIndex];
 }
 
+// Listen for a click or a finger tap.
+// Use preventDefault() in the function to prevent double clicking.
 const logo = document.getElementById("logo");
 logo.addEventListener("click", nextLogoImage);
 logo.addEventListener("touchstart", nextLogoImage);
