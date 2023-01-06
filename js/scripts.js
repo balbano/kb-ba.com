957d3ef5d5f7fdad46f385f95ff1dd6b6d7e4eef
balbano <brendan@brendanalbano.com>
2023-01-06 10:47:23 -0800

    Launch!

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,15 @@
 // src values to rotate through when you click on the logo.
 const logoSrcs = [
     "assets/logo_rotation/logo_600x600.png",
-    "assets/logo_rotation/familiar_600x600.png",
-    "assets/logo_rotation/familiar_closeup_600x600.png"
+    "assets/logo_rotation/spraggins_front_600x600.png",
+    "assets/logo_rotation/spraggins_foundation_600x600.png",
+    "assets/logo_rotation/wallpaper_rocks_600x600.png",
+    "assets/logo_rotation/purringtons_600x600.png",
+    "assets/logo_rotation/middle_school_sketch_600x600.png",
+    "assets/logo_rotation/green_grove_600x600.png",
+    "assets/logo_rotation/casual_stories_project_600x600.png",
+    "assets/logo_rotation/blower_door_600x600.png",
+    "assets/logo_rotation/swing_600x600.png"
 ];
 var logoSrcIndex = 0;
 
